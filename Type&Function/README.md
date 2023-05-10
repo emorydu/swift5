586e59635d9a2a4b370a6295babe29df0d66d167
Emory.Du <Emory163@163.com>
2023-05-10 15:49:15 +0800

    feat(type&function) swift5 basics about type&function & code practice

---
+++ Type&Function/README.md
@@ -0,0 +1,23 @@
+# Type&Function
+## 类型可以做什么
+1. 建立变量时提供，让Swift知道它的大小，寻找合适的内存空间存储资料
+2. 类型可以限制属性/方法
+## 方法&函数
+1. 方法不等同于函数
+## Tuple类型
+1. 由多种资料组成
+2. 简单处理复合类型资料
+3. 通过数字或名字存取个别数值
+```swift
+let fruit = ("Apple", 10)
+print(fruit.0)
+print(fruit.1)
+
+let fruit = (name: "Apple", price: 10)
+print(fruit.name)
+print(fruit.price)
+```
+## typealias
+1. 类型别名
+
+
---
+++ Type&Function/code.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+// Tuple
+let http404Error = (404, "Not Found")
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
